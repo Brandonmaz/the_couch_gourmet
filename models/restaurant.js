3882872ff2acad9853726b61a855747b1ef62ffc
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-14 12:03:34 -0700

    restaurant schema updated to fix bug

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema
 const Posts = require('./posts.js')
 
 
-const userSchema = new Schema ({
-  username:{type:String, unique: true, required: true, validate: {
+const restaurantSchema = new Schema ({
+  name:{type:String, unique: true, required: true, validate: {
                           validator: (str) => {
                             let length = str.length
                             if(length < 5 || length > 16) {
@@ -20,16 +20,7 @@ const userSchema = new Schema ({
                             } else {
                               return true
                             }}, message: 'Your password must be between 7 and 16 characters.'}},
-  about:{type:String, validate: {
-                            validator: (str) => {
-                              for(let i = 0; i < str.length-4; i++) {
-                                let thisThree= str.slice(i, (i+4))
-                                if (thisThree === ('http')) {
-                                  return false
-                                }
-                              }
-                              return true
-                            }, message: 'Your about me section cannot contain any links or images.'}}
+  about:{type:String, required:true}
 })
 
 
