11a3f84c829d7903fa277ac483e50dadaf693e2f
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-14 13:32:12 -0700

    added app.js basic template and newrestaurant.js

---
+++ public/js/newrestaurant.js
@@ -0,0 +1,16 @@
+class NewRestaurantForm extends React.Component {
+
+
+
+  render = () => {
+    return (
+      <h1>New Restaurant Works</h1>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <NewRestaurantForm></NewRestaurantForm>, document.querySelector('#testApp')
+)
