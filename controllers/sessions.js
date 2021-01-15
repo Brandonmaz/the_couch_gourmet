963485452ff12475f6cacbdfcf1002d0ffeb6fbd
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-15 10:07:12 -0700

    user has ability to login in and also will recieve error messages if they enter incorrect info

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,19 +4,20 @@ const sessions = express.Router()
 const Restaurant = require('../models/restaurant.js')
 
 sessions.post('/', (req, res) => {
-  User.findOne({username:req.body.username}, (err, foundUser) => {
+  console.log(req.body.username);
+  Restaurant.findOne({username:req.body.username}, (err, foundUser) => {
     if(err) {
       console.log(err);
-      res.json({})
+      res.json("There was an error. Please try again.")
     } else if (!foundUser) {
       console.log('username does not exist');
-      res.json({})
+      res.json('Username or password is incorrect. Please try again.')
     } else {
       if(bcrypt.compareSync(req.body.password, foundUser.password)) {
         res.json(foundUser)
       } else {
         console.log('We don\'t recognize that password. Try again');
-        res.json({})
+        res.json('We don\'t recognize that username or password. Please try again.')
       }
     }
   })
