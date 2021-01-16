92057e66a312c2dcf34fd830cc846bafdfcdf8c5
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-16 14:32:19 -0700

    restaurants acan upload a phot

--- models/restaurant.js
+++ models/restaurant.js
@@ -14,6 +14,7 @@ const restaurantSchema = new Schema ({
                             }}, message: 'Your name must be between 5 and 16 characters long.'}},
   password:{type:String, required:true},
   about:{type:String, required:true},
+  img: {type:String, default:"https://i.imgur.com/BN3CHjAl.jpg"},
   posts: [Post.schema]
 })
 
