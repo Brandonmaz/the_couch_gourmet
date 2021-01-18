4bfdcdaa28321cb6d990d9b43a4b69d6603ff523
Hunter Wallen <hunterwallen67@gmail.com>
2021-01-17 19:49:05 -0700

    fixed modal issue

--- public/js/restaurantfeed.js
+++ public/js/restaurantfeed.js
@@ -56,29 +56,24 @@ class RestaurantFeed extends React.Component {
   }
 
 
-toggleModal = () => {
+toggleModal = (event) => {
   event.persist()
   /* Get the modal */
-  let modal = document.getElementById("myModal");
+  let modal = event.target.previousSibling;
 
   /* Get the image and insert it inside the modal - use its "alt" text as a caption */
-  let img = document.getElementById("myImg")
-  let mImg = document.getElementById("img01")
-  let cText = document.getElementById("caption")
-  img.onclick = () => {
+
     modal.style.display = "block"
-    mImg.src = this.src
-    cText.innerHTML = this.alt
-  }
+
 }
 toggleSpan = (event) => {
   /* Get the <span> element that closes the modal */
+  let modal = event.target.parentElement
   let span = document.getElementsByClassName("close")[0]
   event.persist()
   /* When the user clicks on <span> (x), close the modal */
-  span.onclick = () => {
-    modal.style.display = "none"
-  }
+  modal.style.display = "none"
+
 }
   render = () => {
     return (
@@ -87,34 +82,41 @@ toggleSpan = (event) => {
           {this.props.restaurants.map((restaurant) => {
             return(
               <div className="individualContent">
-                <h2 id="restName">{restaurant.username}</h2>
-                <img id="myImg" src={restaurant.img} alt={restaurant.about} onClick={this.toggleModal}/>
-                <div id="myModal" className="modal">
-                  <span className="close" onClick={this.toggleSpan}>&times;</span>
-                  <img className="modal-content" id="img01" src={restaurant.img}/>
-                  <div id="caption"></div>
-                </div>
+                <div className="singleRestaurant">
+                  <h2 id="restName">{restaurant.username}</h2>
+                  <div className="imageModalDiv">
+                    <div id="myModal" className="modal" style={{display: 'none'}}>
+                      <span className="close" onClick={this.toggleSpan}>X</span>
+                      <div className='modalContent'>
+                        <h2 className='modalTitle'>About {restaurant.username}</h2>
+                        <h4 className="caption">{restaurant.about}</h4>
+                      </div>
+                    </div>
+                    <img id="myImg" src={restaurant.img} alt={restaurant.about} onClick={this.toggleModal}/>
+                  </div>
                   <button className='viewReviews myButton' _id={restaurant._id} onClick={this.toggleReviews}>Reviews</button>
                   <ul id="reviewList" style={{display:'none'}}>
-                    {restaurant.posts.map((post) => {
-                      return(
-                        <li key={post._id}>
-                          <h4>{post.title}</h4>
-                          <h4>{post.stars} Stars</h4>
-                          <h6>By: {post.author}</h6>
-                          <h5>{post.body}</h5>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                  {(this.props.sessions.currentUser._id !== "") ?
-                    <div id='allowPostsDiv'>
-                        <button className="myButton" id='addReview' _id={restaurant._id} onClick={this.toggleAdd}>Post a Review</button>
-                        <div id='reviewFormDiv' style={{display: 'none'}}>
-                         <NewPostForm sessions={this.props.sessions} restaurantName={restaurant.username} restaurantId={restaurant._id} createPost={this.createPost}></NewPostForm>
-                         </div>
-                     </div>
-                     : null}
+                      {restaurant.posts.map((post) => {
+                        return(
+                          <li key={post._id}>
+                            <h4>{post.title}</h4>
+                            <h4>{post.stars} Stars</h4>
+                            <h6>By: {post.author}</h6>
+                            <h5>{post.body}</h5>
+                          </li>
+                        )
+                      })}
+                    </ul>
+
+                    {(this.props.sessions.currentUser._id !== "") ?
+                      <div id='allowPostsDiv'>
+                          <button className="myButton" id='addReview' _id={restaurant._id} onClick={this.toggleAdd}>Post a Review</button>
+                          <div id='reviewFormDiv' style={{display: 'none'}}>
+                           <NewPostForm sessions={this.props.sessions} restaurantName={restaurant.username} restaurantId={restaurant._id} createPost={this.createPost}></NewPostForm>
+                           </div>
+                       </div>
+                       : null}
+                </div>
               </div>
             )})}
         </div>
